007a5720fd29f4fe396fd5e469622a227ad055b6
paul875 <34162649+paul875@users.noreply.github.com>
2021-12-02 19:04:58 +0300

    Add files via upload

---
+++ MyPytest_second/tests/second_test.py
@@ -0,0 +1,14 @@
+from app.calculator import Calculator
+import pytest
+
+class TestCalc:
+    def setup(self):
+        self.calc=Calculator
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self,3,3) == 9
+    def test_substraction_calculate_correctly(self):
+        assert self.calc.subtraction(self,5,2) == 3
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self,6,3) == 2
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self,10,20) == 30
